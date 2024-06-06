2cfdccbf3ae38dfae96b12005e49b5cd294e517c
Leonardo Acquaroli <leoacqua2010.la@gmail.com>
2024-06-06 12:11:28 +0200

    added multi message and icon

--- cv_bot_app.py
+++ cv_bot_app.py
@@ -14,11 +14,15 @@ import streamlit as st
 
 load_dotenv()
 
+st.title("Chat with Leo👋🏻")
+
 LANGCHAIN_TRACING_V2 = os.getenv("LANGCHAIN_TRACING_V2")
 LANGCHAIN_API_KEY = os.getenv("LANGCHAIN_API_KEY")
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 MODEL = "gpt-3.5-turbo"
+
+avatar_image_url = 'https://raw.githubusercontent.com/LeonardoAcquaroli/cv-bot/main/avatar.webp'
 
 #Openai Embeddings
 embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
@@ -27,17 +31,37 @@ embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
 current_dir = os.path.dirname(os.path.abspath(__file__))
 doc_folder = os.path.join(current_dir, 'docs')
 
-# User query
-query = st.chat_input(placeholder='Ask about Leonardo')
-
 # Initialize the ChatOpenAI instance with your model and API key
 llm = ChatOpenAI(model=MODEL, temperature=0.000001, api_key=OPENAI_API_KEY)
 
-if query:
-    
-    response, search_result = handle_user_query(query=query,
-                                                embeddings=embeddings,
-                                                llm=llm)
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-    with st.chat_message(name='user'):
-        st.write(response)
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"], avatar = avatar_image_url if message["role"] == "assistant" else None):
+        st.markdown(message["content"])
+
+@st.cache_resource
+def display_chat_message(role, content):
+    '''
+    Display chat message in the chat message container and add it to chat history through session state.
+    '''
+    # Display user message in chat message container
+    with st.chat_message(role):
+        st.markdown(content)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": role, "content": content})
+
+# React to user input
+if prompt := st.chat_input(placeholder='Ask about Leonardo'):
+
+    display_chat_message("user", prompt)
+
+    # Chatbot response
+    response, search_result = handle_user_query(query=prompt,
+                                            embeddings=embeddings,
+                                            llm=llm)
+    
+    display_chat_message("assistant", response)
