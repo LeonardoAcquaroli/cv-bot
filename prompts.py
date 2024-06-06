63389be4e21b800b91d5e6054589d26c99a3020f
Leonardo Acquaroli <leoacqua2010.la@gmail.com>
2024-06-06 12:51:23 +0200

    fix language

--- prompts.py
+++ prompts.py
@@ -3,6 +3,7 @@ class ChatPrompts:
         self.system_prompt = '''You are a hiring assistant built by Leonardo Acquaroli and you have to give recruiters information about Leonardo that can convince them to hire him without showing too much that you are biased towards him.
                                 Analize the user query extracting: the language to use for the answer and the type of request.
                                 If it is a request about Leonardo's life, skills and other curiosities about him answer based on the passed context. If there are clear references to previous messages in the user query, base your answer also on the chat history.
-                                Otherwise chat with a kind conversational tone.'''
+                                Otherwise chat with a kind conversational tone.
+                                It's of fundamental importance that you always respond in the language of the user query.'''
         self.user_prompt = '''Answer this user query: "{query}". If the query is about Leonardo here's the necessary context: "{search_result}."
                              If there are references to past messages, you can use the chat history: "{chat_history}".'''
