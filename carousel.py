782282ab81474af658cae4d3d5c853a78d0411bb
Leonardo Acquaroli <leoacqua2010.la@gmail.com>
2024-06-06 13:57:16 +0200

    commented default links

--- carousel.py
+++ carousel.py
@@ -5,13 +5,13 @@ carousel_items = [
         title="iDays win 2022",
         text="The Quokkay team wins the iDays 2022 Hackathon",
         img="https://raw.githubusercontent.com/LeonardoAcquaroli/cv-bot/main/images/quokkay.jpeg",
-        link="https://discuss.streamlit.io/t/new-component-react-bootstrap-carousel/46819"
+        # link="https://discuss.streamlit.io/t/new-component-react-bootstrap-carousel/46819"
     ),
     dict(
         title="Big data for Gender equality",
         text="Me and Melissa, presenting our project in Auditorium Cascina Triulza at MIND in Milan",
         img="https://raw.githubusercontent.com/LeonardoAcquaroli/cv-bot/main/images/mind.jpeg",
-        link="https://github.com/thomasbs17/streamlit-contributions/tree/master"
+        # link="https://github.com/thomasbs17/streamlit-contributions/tree/master"
     ),
     dict(
         title="Playing football",
